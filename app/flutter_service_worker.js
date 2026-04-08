ab3d97c73c2a96b52e6d9d1f69b52da03db81f5f
emir683 <67478516+emir683@users.noreply.github.com>
2026-04-08 21:53:02 +0300

    Publish web build with corrected Firebase web key

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {".well-known/assetlinks.json": "9321e6b386b9fd629d201aa7bd6ba
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "a101e4cdef5c85a8a84aad4d995dc8d5",
+"flutter_bootstrap.js": "b980cc7859802dd36a088039b6812de4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -43,7 +43,7 @@ const RESOURCES = {".well-known/assetlinks.json": "9321e6b386b9fd629d201aa7bd6ba
 "index.html": "dedc69a05b533cb3988ed825f29c2159",
 "/": "dedc69a05b533cb3988ed825f29c2159",
 "katil/index.html": "35962bc6106222dd32adf3b7aea9be79",
-"main.dart.js": "606fd6ebed594c0600b5600f2b74b68f",
+"main.dart.js": "6576cfe1fdf10c624181d8d664aba84d",
 "manifest.json": "45ca3efbe187302e4e2d2e3e246b73ab",
 "version.json": "6140f752e456dccb11e266aa2707b4c1"};
 // The application shell files that are downloaded before a service worker can
